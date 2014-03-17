47dd407321ac8cb4811209fff09c4bd802b14bf2
Anvit <anvit.tawar@gmail.com>
2014-03-17 16:24:36 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dizzert::Application.config.secret_token = '142bc6078cad49c23c2212625ed0a79fd3b736aa977dc17113311e51d9e32b96b113d7e8481ced98948fbdfcf489b45b3c49a5921fd93038978b597566420dfb'
